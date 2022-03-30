e7e3820276cd47cd34d8272c27c8f9395045e62b
Jasmin Ovcina <70064805+ovciina@users.noreply.github.com>
2022-03-30 18:33:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1
-console.log(secretNumber);
 let score = 20;
 let highscore = 0;
 
